c97c26ebc5e44ae152d0bef648b1785943d76cca
Luka <lukagaxokidze28@gmail.com>
2024-07-26 01:38:22 +0400

    UI

--- src/app/components/product-page/product-page.component.ts
+++ src/app/components/product-page/product-page.component.ts
@@ -1,14 +1,15 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink, RouterModule } from '@angular/router';
 import { SingleProductService } from '../../shared/services/single-product.service';
 import { Products } from '../../shared';
 import { SingleProduct } from '../../shared/interface/singleProduct';
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-product-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterModule],
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.scss']
 })
