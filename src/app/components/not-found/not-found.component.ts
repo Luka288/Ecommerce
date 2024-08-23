d0dd8151ef7056608321230a93a7029a4afe3e07
Luka <lukagaxokidze28@gmail.com>
2024-08-23 11:47:13 +0400

    not found page
    added timer when opening page it will redirect user in 5 second to main page

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
@@ -7,6 +8,23 @@ import { Component } from '@angular/core';
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss'
 })
-export default class NotFoundComponent {
+export default class NotFoundComponent implements OnInit {
+  private readonly route = inject(Router)
+  private readonly ref = inject(ChangeDetectorRef)
+  
+  counter: number = 5;
 
+  ngOnInit(): void {
+    this.backToHome()
+  }
+
+  backToHome(){ 
+    const interval = setInterval(() => {
+        this.counter--;
+        if(this.counter === 0){
+          clearInterval(interval);
+          this.route.navigateByUrl('/')
+        }
+    }, 1000);
+  }
 }
